e5556be101afe87d32e9bdc45cdba8172ba571e7
gioadami <giordano.adami@gmail.com>
2014-11-17 17:29:02 +0100

    PromotedJourneyRequestConverter.

---
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/DummyPromotedJourneyRequestConverter.java
@@ -0,0 +1,19 @@
+package eu.trentorise.smartcampus.mobility.controller.extensions;
+
+import it.sayservice.platform.smartplanner.data.message.journey.SingleJourney;
+
+import java.util.List;
+
+
+public class DummyPromotedJourneyRequestConverter implements PromotedJourneyRequestConverter {
+
+	@Override
+	public void modifyRequest(SingleJourney request) {
+		
+	}	
+	
+	@Override
+	public void promoteJourney(List<PlanRequest> requests) {
+	}	
+	
+}
---
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/PromotedJourneyRequestConverter.java
@@ -0,0 +1,12 @@
+package eu.trentorise.smartcampus.mobility.controller.extensions;
+
+import it.sayservice.platform.smartplanner.data.message.journey.SingleJourney;
+
+import java.util.List;
+
+
+public interface PromotedJourneyRequestConverter {
+
+	public void modifyRequest(SingleJourney request);
+	public void promoteJourney(List<PlanRequest> requests);
+}
