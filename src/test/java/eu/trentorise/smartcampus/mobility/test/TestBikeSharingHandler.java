b8cb2851484296dc15eba5ac71a79f1c5c90f3f0
gioadami <giordano.adami@gmail.com>
2015-01-26 14:28:40 +0100

    Implemented tests.

--- src/test/java/eu/trentorise/smartcampus/mobility/test/TestBikeSharingHandler.java
+++ src/test/java/eu/trentorise/smartcampus/mobility/test/TestBikeSharingHandler.java
@@ -16,11 +16,104 @@
 
 package eu.trentorise.smartcampus.mobility.test;
 
+import static org.junit.Assert.assertEquals;
+import it.sayservice.platform.smartplanner.data.message.Itinerary;
+import it.sayservice.platform.smartplanner.data.message.Leg;
+
+import java.util.Collections;
+
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.google.common.collect.Lists;
+
+import eu.trentorise.smartcampus.mobility.model.Parking;
+import eu.trentorise.smartcampus.mobility.service.AlertSender;
+import eu.trentorise.smartcampus.mobility.storage.DomainStorage;
+import eu.trentorise.smartcampus.mobility.storage.ItineraryObject;
+
 /**
  * @author raman
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class})
 public class TestBikeSharingHandler {
 
-	// TODO
+	@Autowired
+	private DomainStorage storage;
+
+	@Autowired
+	private AlertSender alertSender;
+	
+	@Before
+	public void init() {
+		storage.reset();
+	}
+	
+	@Test
+	public void testVehicles() {
+		Itinerary bikeSharing = ObjectCreator.createWithBikeSharing();
+		String id = new ObjectId().toString();
+		ItineraryObject io = new ItineraryObject("1", id, bikeSharing, bikeSharing.getFrom(), bikeSharing.getTo(), "test");
+		io.setMonitor(true);
+		storage.saveItinerary(io);
+
+		// many vehicles available
+		Parking fromStation = ObjectCreator.createBikeSharingFrom(10);
+		alertSender.publishParkings(Lists.asList(fromStation, new Parking[0]));
+		io = storage.searchDomainObject(Collections.<String,Object>singletonMap("clientId", id), ItineraryObject.class);
+		long found = hasFewVehiclesAvailableAlert(io);
+		assertEquals(-1, found);
+		
+		// few vehicles available
+		fromStation = ObjectCreator.createBikeSharingFrom(2);
+		alertSender.publishParkings(Lists.asList(fromStation, new Parking[0]));
+		io = storage.searchDomainObject(Collections.<String,Object>singletonMap("clientId", id), ItineraryObject.class);
+		found = hasFewVehiclesAvailableAlert(io);
+		assertEquals(2, found);		
+	}
+	
+	@Test
+	public void testPlaces() {
+		Itinerary bikeSharing = ObjectCreator.createWithBikeSharing();
+		String id = new ObjectId().toString();
+		ItineraryObject io = new ItineraryObject("1", id, bikeSharing, bikeSharing.getFrom(), bikeSharing.getTo(), "test");
+		io.setMonitor(true);
+		storage.saveItinerary(io);
+
+		// many places available
+		Parking toStation = ObjectCreator.createBikeSharingTo(10);
+		alertSender.publishParkings(Lists.asList(toStation, new Parking[0]));
+		io = storage.searchDomainObject(Collections.<String,Object>singletonMap("clientId", id), ItineraryObject.class);
+		long found = hasFewPlacesAvailableAlert(io);
+		assertEquals(-1, found);
+		
+		// few places available
+		toStation = ObjectCreator.createBikeSharingTo(1);
+		alertSender.publishParkings(Lists.asList(toStation, new Parking[0]));
+		io = storage.searchDomainObject(Collections.<String,Object>singletonMap("clientId", id), ItineraryObject.class);
+		found = hasFewPlacesAvailableAlert(io);
+		assertEquals(1, found);				
+	}	
+	
+	private long hasFewPlacesAvailableAlert(ItineraryObject io) {
+		for (Leg leg : io.getData().getLeg()) {
+			if (leg.getAlertParkingList().size() > 0) return leg.getAlertParkingList().get(0).getPlacesAvailable(); 
+		}
+		return -1L;
+	}	
+	
+	private long hasFewVehiclesAvailableAlert(ItineraryObject io) {
+		for (Leg leg : io.getData().getLeg()) {
+			if (leg.getAlertParkingList().size() > 0) return leg.getAlertParkingList().get(0).getNoOfvehicles();
+		}
+		return -1L;
+	}		
+	
 }
