3564b5a1757b632eb4f3e717772a13e115932c31
gioadami <giordano.adami@gmail.com>
2015-04-29 18:07:20 +0200

    maxTotalWalkDistance only for some transports.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/TrentoPromotedJourneyRequestConverter.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/TrentoPromotedJourneyRequestConverter.java
@@ -16,7 +16,8 @@ public class TrentoPromotedJourneyRequestConverter implements PromotedJourneyReq
 			if (pr.getType().equals(TType.CARWITHPARKING)) {
 				pr.setRequest(pr.getRequest() + "&extraTransport=WALK");
 			}
-			if (!pr.getType().equals(TType.WALK)) {
+			if (pr.getType().equals(TType.SHAREDBIKE) ||pr.getType().equals(TType.SHAREDBIKE_WITHOUT_STATION) || pr.getType().equals(TType.SHAREDCAR) || pr.getType().equals(TType.SHAREDCAR_WITHOUT_STATION)
+					|| pr.getType().equals(TType.CARWITHPARKING) || pr.getType().equals(TType.PARK_AND_RIDE)) {
 				pr.setRequest(pr.getRequest() + "&maxTotalWalkDistance=1250");
 			}
 		}
