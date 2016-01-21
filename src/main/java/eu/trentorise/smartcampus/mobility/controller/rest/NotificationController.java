25a6da39c02919e13004f8e0b78a9193238dc66f
gioadami <giordano.adami@gmail.com>
2016-01-21 14:15:53 +0100

    Announcements.

---
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/NotificationController.java
@@ -0,0 +1,53 @@
+package eu.trentorise.smartcampus.mobility.controller.rest;
+
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Maps;
+
+import eu.trentorise.smartcampus.mobility.model.Announcement;
+import eu.trentorise.smartcampus.mobility.processor.alerts.AlertNotifier;
+import eu.trentorise.smartcampus.mobility.security.AppDetails;
+
+@Controller
+@RequestMapping(value = "/notification")
+public class NotificationController {
+
+	@Autowired
+	private AlertNotifier notifier;		
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String notify(HttpSession session) {
+		return "notification";
+	}	
+
+	@RequestMapping(method = RequestMethod.POST, value = "notify")
+	public @ResponseBody Map<String, String> notify(@RequestBody Announcement announcement, HttpServletResponse response) throws Exception {
+		Map<String, String> result = Maps.newTreeMap();
+
+		try {
+
+			String appId = ((AppDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getApp().getMessagingAppId();
+			
+			notifier.notifyAnnouncement(announcement, appId);
+
+			result.put("message", "Message \"" + announcement.getTitle() + "\" sent @ " + new Date());
+		} catch (Exception e) {
+			result.put("error", "Exception @ " + new Date() + ": " + e.toString());
+		}
+
+		return result;
+	}
+	
+}
---
+++ src/main/java/eu/trentorise/smartcampus/mobility/model/Announcement.java
@@ -0,0 +1,33 @@
+package eu.trentorise.smartcampus.mobility.model;
+
+public class Announcement {
+
+	private String title;
+	private String description;
+	private String appId;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+}
