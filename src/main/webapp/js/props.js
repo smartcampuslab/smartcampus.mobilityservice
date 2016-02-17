54bf68bbd31abcea98366a2d6707ccb310042ff5
Роман <kazhamiakin@gmail.com>
2016-02-17 12:48:28 +0100

    fixed schema

--- src/main/webapp/js/props.js
+++ src/main/webapp/js/props.js
@@ -2,8 +2,8 @@ MAIL='webplanner@smartcommunitylab.it';
 
 GEOCODER = 'https://os.smartcommunitylab.it/core.geocoder/';
 PARKING = 'https://dev.smartcommunitylab.it/bologna.mobility/';
-BIKESHARING = 'http://dev.smartcommunitylab.it/bologna.mobility/';
-PLANNER = 'http://dev.smartcommunitylab.it/bologna.mobility';
+BIKESHARING = 'https://dev.smartcommunitylab.it/bologna.mobility/';
+PLANNER = 'https://dev.smartcommunitylab.it/bologna.mobility';
 PARKING_AGENCIES = [];
 EXTRAURBAN_AGENCIES=[];
 BIKE_AGENCIES = [];
