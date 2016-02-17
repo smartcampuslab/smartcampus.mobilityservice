208266a726d1157901d0ac441d534d17f9c70daf
Роман <kazhamiakin@gmail.com>
2016-02-17 12:33:41 +0100

    app url

--- src/main/webapp/js/props.js
+++ src/main/webapp/js/props.js
@@ -1,9 +1,9 @@
 MAIL='webplanner@smartcommunitylab.it';
 
 GEOCODER = 'https://os.smartcommunitylab.it/core.geocoder/';
-PARKING = 'http://localhost:8080/core.mobility/';
-BIKESHARING = 'http://localhost:8080/core.mobility/';
-PLANNER = 'http://localhost:8080/core.mobility';
+PARKING = 'http://localhost:8080/bologna.mobility/';
+BIKESHARING = 'http://localhost:8080/bologna.mobility/';
+PLANNER = 'http://localhost:8080/bologna.mobility';
 PARKING_AGENCIES = [];
 EXTRAURBAN_AGENCIES=[];
 BIKE_AGENCIES = [];
