53580b34343bf9e5fd052c8f5c58773de4592b1a
gioadami <giordano.adami@gmail.com>
2016-02-25 16:40:20 +0100

    Itinerary recurrency.

---
+++ src/main/java/eu/trentorise/smartcampus/mobility/model/ItineraryRecurrency.java
@@ -0,0 +1,18 @@
+package eu.trentorise.smartcampus.mobility.model;
+
+import java.util.Set;
+
+public class ItineraryRecurrency {
+
+	private Set<Integer> daysOfWeek;
+
+	public Set<Integer> getDaysOfWeek() {
+		return daysOfWeek;
+	}
+
+	public void setDaysOfWeek(Set<Integer> daysOfWeek) {
+		this.daysOfWeek = daysOfWeek;
+	}
+	
+	
+}
