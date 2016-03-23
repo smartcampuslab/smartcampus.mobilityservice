a5834082d7fd30c43c8f86531f9967f72c121510
gioadami <giordano.adami@gmail.com>
2016-03-23 10:57:31 +0100

    Wheelchair.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/PlanRequest.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/PlanRequest.java
@@ -96,10 +96,6 @@ public class PlanRequest {
 	public void setWheelChair(boolean wheelChair) {
 		this.wheelChair = wheelChair;
 	}
-	
-	public boolean isWheelChair() {
-		return wheelChair;
-	}
 
 	@Override
 	public String toString() {
