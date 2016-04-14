9cb5dafb72a4fd8d782fe22d90036800fc0ee54c
gioadami <giordano.adami@gmail.com>
2016-04-14 16:59:38 +0200

    processRequests as Trento.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/RoveretoPromotedJourneyRequestConverter.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/extensions/RoveretoPromotedJourneyRequestConverter.java
@@ -1,6 +1,7 @@
 package eu.trentorise.smartcampus.mobility.controller.extensions;
 
 import it.sayservice.platform.smartplanner.data.message.Position;
+import it.sayservice.platform.smartplanner.data.message.RType;
 import it.sayservice.platform.smartplanner.data.message.TType;
 import it.sayservice.platform.smartplanner.data.message.journey.SingleJourney;
 
@@ -43,6 +44,32 @@ public class RoveretoPromotedJourneyRequestConverter implements PromotedJourneyR
 	public void processRequests(List<PlanRequest> requests, int iteration) {
 		for (PlanRequest pr: requests) {
 			pr.setRequest("");
+			if (pr.getType().equals(TType.CARWITHPARKING)) {
+				pr.setRequest(pr.getRequest() + "&extraTransport=WALK");
+			}
+			if (pr.getType().equals(TType.TRANSIT) || pr.getType().equals(TType.BUS)) {
+				if (pr.getRouteType().equals(RType.leastWalking) && iteration == 0) {
+					pr.setRequest(pr.getRequest() + "&maxWalkDistance=500");
+					pr.setRetryOnFail(true);
+				} else {
+					pr.setRequest(pr.getRequest() + "&maxWalkDistance=1000");
+					pr.setRetryOnFail(false);
+				}
+			}			
+			if (pr.getType().equals(TType.SHAREDBIKE) || pr.getType().equals(TType.SHAREDBIKE_WITHOUT_STATION) || pr.getType().equals(TType.SHAREDCAR) || pr.getType().equals(TType.SHAREDCAR_WITHOUT_STATION)
+					|| pr.getType().equals(TType.CARWITHPARKING) || pr.getType().equals(TType.PARK_AND_RIDE)) {
+				pr.setRequest(pr.getRequest() + "&maxTotalWalkDistance=1250");
+			}
+			if (pr.getType().equals(TType.SHAREDBIKE) || pr.getType().equals(TType.SHAREDBIKE_WITHOUT_STATION) || pr.getType().equals(TType.BICYCLE)) {
+				pr.setRouteType(RType.safest);
+			}
+			if (pr.getType().equals(TType.WALK) && pr.getValue() != 0) {
+				if (pr.getRouteType().equals(RType.leastWalking)) {
+					pr.setRequest(pr.getRequest() + "&maxTotalWalkDistance=500");
+				} else {
+					pr.setRequest(pr.getRequest() + "&maxTotalWalkDistance=1000");
+				}
+			}
 		}
 	}
 	
