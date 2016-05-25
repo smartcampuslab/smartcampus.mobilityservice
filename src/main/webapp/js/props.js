71b103e7c116b2271a2d58eef917447bb073bd26
gioadami <giordano.adami@gmail.com>
2016-05-25 10:23:47 +0200

    Fixed URLs.

--- src/main/webapp/js/props.js
+++ src/main/webapp/js/props.js
@@ -1,11 +1,11 @@
 MAIL='webplanner@smartcommunitylab.it';
 
 GEOCODER = 'https://os.smartcommunitylab.it/core.geocoder/';
-PARKING = 'http://localhost:8080/core.mobility/';
-TAXI = 'http://localhost:8080/core.mobility/';
-BIKESHARING = 'http://localhost:8080/core.mobility/';
-PLANNER = 'http://localhost:8080/core.mobility';
+PARKING = 'https://dev.smartcommunitylab.it/core.mobility/';
+TAXI = 'https://dev.smartcommunitylab.it/core.mobility/';
+BIKESHARING = 'https://tn.smartcommunitylab.it/core.mobility/';
+PLANNER = 'https://dev.smartcommunitylab.it/core.mobility';
 PARKING_AGENCIES = ['COMUNE_DI_TRENTO','COMUNE_DI_ROVERETO'];
 EXTRAURBAN_AGENCIES=['17'];
 BIKE_AGENCIES = ['trento','rovereto', 'pergine_valsugana'];
-CENTER = [46.069015, 11.128830];
+CENTER = [45.889901, 11.040521];
