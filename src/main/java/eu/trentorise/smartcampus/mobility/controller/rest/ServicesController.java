9cb7bd3d5b593ec6c54201737f5faf9a51d59a8e
gioadami <giordano.adami@gmail.com>
2016-10-14 17:11:26 +0200

    New methods.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/ServicesController.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/ServicesController.java
@@ -2,6 +2,7 @@ package eu.trentorise.smartcampus.mobility.controller.rest;
 
 import it.sayservice.platform.smartplanner.data.message.alerts.AlertDelay;
 import it.sayservice.platform.smartplanner.data.message.alerts.AlertParking;
+import it.sayservice.platform.smartplanner.data.message.alerts.AlertRoad;
 
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,18 +20,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 
-import eu.trentorise.smartcampus.mobility.service.NewAlertSender;
+import eu.trentorise.smartcampus.mobility.model.Station;
+import eu.trentorise.smartcampus.mobility.processor.handlers.BikeSharingCache;
+import eu.trentorise.smartcampus.mobility.service.AlertSender;
 
 @Controller
 public class ServicesController {
 	
 	@Autowired
-	private NewAlertSender alertSender;	
+	private AlertSender alertSender;	
 	
-	@RequestMapping(method = RequestMethod.GET, value = "servicedata/ping")
-	public @ResponseBody void pushAlertParkings(HttpServletResponse response) throws Exception {
-		System.out.println("OK");
-	}
+	@Autowired
+	private BikeSharingCache bikeSharingCache;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "servicedata/publishAlertParkings")
 	public @ResponseBody void pushAlertParkings(HttpServletResponse response, @RequestBody List<Map> data) throws Exception {
@@ -50,8 +52,31 @@ public class ServicesController {
 			AlertDelay ap = mapper.convertValue(map, AlertDelay.class);
 			alerts.add(ap);
 		}
-		alertSender.publishTrains(null);
-	}	
-	
+        alertSender.publishTrains(alerts);
+    }    
+    
+    @RequestMapping(method = RequestMethod.POST, value = "servicedata/publishAlertRoads")
+    public @ResponseBody void pushAlertRoads(HttpServletResponse response, @RequestBody List<Map> data) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<AlertRoad> alerts = Lists.newArrayList();
+        for (Map map: data) {
+            AlertRoad ar = mapper.convertValue(map, AlertRoad.class);
+            alerts.add(ar);
+        }
+        alertSender.publishRoadWorkAlerts(alerts);
+    }    
+
+    @RequestMapping(method = RequestMethod.POST, value = "servicedata/publishBikeStations/{comune}/{agencyId}")
+    public @ResponseBody void publishBikeStations(HttpServletResponse response, @RequestBody List<Map> data, @PathVariable String comune, @PathVariable String agencyId) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Station> stations = Lists.newArrayList();
+        for (Map map: data) {
+        	Station s = mapper.convertValue(map, Station.class);
+            stations.add(s);
+        }
+        bikeSharingCache.setStations(comune, agencyId, stations);
+    } 	
+    
+    
 	
 }
