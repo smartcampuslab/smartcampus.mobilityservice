675c65da205bcf5d250396e11daa505a8b79aed7
kazhamiakin <kazhamiakin@gmail.com>
2017-09-26 15:04:25 +0200

    fix for actinide

--- src/main/java/eu/trentorise/smartcampus/mobility/gamification/GamificationManager.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/gamification/GamificationManager.java
@@ -269,7 +269,7 @@ public class GamificationManager {
 		ExecutionDataDTO ed = new ExecutionDataDTO();
 		ed.setGameId(game.getId());
 		ed.setPlayerId(id);
-		ed.setActionId("checkin"+event);
+		ed.setActionId("checkin_"+event);
 		ed.setData(Collections.singletonMap("checkinType",event));
 
 		String content = JsonUtils.toJSON(ed);
