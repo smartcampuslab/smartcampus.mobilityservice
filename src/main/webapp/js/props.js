021972b14d28c5d95cf578ed9ae94d90699c5672
gioadami <giordano.adami@gmail.com>
2017-11-17 15:01:04 +0100

    New bikestation agencies.

--- src/main/webapp/js/props.js
+++ src/main/webapp/js/props.js
@@ -7,5 +7,5 @@ BIKESHARING = 'https://tn.smartcommunitylab.it/core.mobility/';
 PLANNER = 'http://localhost:1010/core.mobility';
 PARKING_AGENCIES = ['COMUNE_DI_TRENTO','COMUNE_DI_ROVERETO'];
 EXTRAURBAN_AGENCIES=['17'];
-BIKE_AGENCIES = ['trento','rovereto', 'pergine_valsugana'];
+BIKE_AGENCIES = ['trento','rovereto', 'pergine_valsugana', 'lavis', 'mezzocorona', 'mezzolombardo', 'sanmichelealladige'];
 CENTER = [46.069015, 11.128830];
