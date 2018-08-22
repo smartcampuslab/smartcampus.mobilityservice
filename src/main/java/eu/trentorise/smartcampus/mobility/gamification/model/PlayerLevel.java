1f6e2e253ba5fe97d1e3e97b5542ef360ade5317
gioadami <giordano.adami@gmail.com>
2018-08-22 14:26:56 +0200

    Updated.

--- src/main/java/eu/trentorise/smartcampus/mobility/gamification/model/PlayerLevel.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/gamification/model/PlayerLevel.java
@@ -1,35 +1,47 @@
 package eu.trentorise.smartcampus.mobility.gamification.model;
 
 public class PlayerLevel {
-	private String levelName;
-	private String levelValue;
-	private String pointConcept;
-	private double toNextLevel;
+    private String levelName;
+    private String levelValue;
+    private String pointConcept;
+    private double startLevelScore;
+    private double endLevelScore;
+    private double toNextLevel;
 
-	public PlayerLevel() {
-	}
+    public PlayerLevel() {
+    }
+    
+    public PlayerLevel(String levelName, String pointConcept, String levelValue,
+            double toNextLevel, double startLevelScore, double endLevelScore) {
+        this.levelName = levelName;
+        this.pointConcept = pointConcept;
+        this.levelValue = levelValue;
+        this.toNextLevel = toNextLevel;
+        this.startLevelScore = startLevelScore;
+        this.endLevelScore = endLevelScore;
+    }
 
-	public PlayerLevel(String levelName, String pointConcept, String levelValue, double toNextLevel) {
-		this.levelName = levelName;
-		this.pointConcept = pointConcept;
-		this.levelValue = levelValue;
-		this.toNextLevel = toNextLevel;
-	}
+    public String getLevelName() {
+        return levelName;
+    }
 
-	public String getLevelName() {
-		return levelName;
-	}
+    public String getPointConcept() {
+        return pointConcept;
+    }
 
-	public String getPointConcept() {
-		return pointConcept;
-	}
+    public double getToNextLevel() {
+        return toNextLevel;
+    }
 
-	public double getToNextLevel() {
-		return toNextLevel;
-	}
+    public String getLevelValue() {
+        return levelValue;
+    }
 
-	public String getLevelValue() {
-		return levelValue;
-	}
+    public double getStartLevelScore() {
+        return startLevelScore;
+    }
 
+    public double getEndLevelScore() {
+        return endLevelScore;
+    }
 }
