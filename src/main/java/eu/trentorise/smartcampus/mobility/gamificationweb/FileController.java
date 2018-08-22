782cd73998b9023529178c1707c8d674f404f973
gioadami <giordano.adami@gmail.com>
2018-08-22 11:14:25 +0200

    Default avatar and appId.

--- src/main/java/eu/trentorise/smartcampus/mobility/gamificationweb/FileController.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/gamificationweb/FileController.java
@@ -1,5 +1,8 @@
 package eu.trentorise.smartcampus.mobility.gamificationweb;
 
+import java.io.InputStream;
+
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.common.io.Resources;
+
 import eu.trentorise.smartcampus.mobility.gamificationweb.model.Avatar;
 import eu.trentorise.smartcampus.mobility.gamificationweb.model.Player;
 import eu.trentorise.smartcampus.mobility.security.AppSetup;
@@ -30,6 +35,8 @@ public class FileController {
 //	@Value("${imagesDir}")
 //	private String imagesDir;
 
+	private static final String DEFAULT_USER = "default_user";
+
 	@Autowired
 	private AppSetup appSetup;
 
@@ -41,6 +48,26 @@ public class FileController {
 
 	private static Log logger = LogFactory.getLog(FileController.class);
 
+	@PostConstruct
+	public void init() throws Exception {
+		Avatar avatar = avatarRepository.findOne(DEFAULT_USER);
+		if (avatar == null) {
+			avatar = new Avatar();
+			
+			InputStream is = Resources.asByteSource(Resources.getResource(DEFAULT_USER + ".png")).openBufferedStream();
+			
+			byte cb[] = ImageUtils.compressImage(is, "image/png");
+			
+			Binary bb = new Binary(cb);
+			avatar.setId(DEFAULT_USER);
+			avatar.setAvatarData(bb);
+			avatar.setContentType("image/png");
+			avatar.setFileName("default.png");
+
+			avatarRepository.save(avatar);
+		}
+	}
+	
 	@PostMapping("/gamificationweb/player/avatar")
 	public @ResponseBody void uploadPlayerAvatar(@RequestParam("data") MultipartFile data, @RequestHeader(required = true, value = "appId") String appId, HttpServletResponse response)
 			throws Exception {
@@ -83,37 +110,17 @@ public class FileController {
 				return;				
 			}
 			
-
-//			if (player.getAvatar() != null) {
-//				deleteFile(imagesDir, player.getAvatar());
-//			}
-
-//			String avatar = saveFile(imagesDir, userId, data);
-//
-//			player.setAvatar(avatar);
-//			playerRepository.save(player);
-
 			Avatar av = new Avatar();
-			
-//			System.err.println("B:" + data.getBytes().length);
 			
 			byte cb[] = ImageUtils.compressImage(data.getInputStream(), data.getContentType());
 
-//			System.err.println("A:" + cb.length);
-			
 			Binary bb = new Binary(cb);
-			av.setId(userId);
+			av.setId(userId + "@" + appId);
 			av.setAvatarData(bb);
 			av.setContentType(data.getContentType());
 			av.setFileName(data.getOriginalFilename());
 
 			avatarRepository.save(av);
-			
-			
-//			response.getOutputStream().write(av.getAvatarData().getData());
-//			response.setContentLength(av.getAvatarData().getData().length);
-//			response.setContentType(av.getContentType());			
-			
 		} catch (Exception e) {
 			logger.error("Error in post avatar: " + e.getMessage(), e);
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -121,67 +128,19 @@ public class FileController {
 
 	}
 
-//	@GetMapping("/gamificationweb/player/avatar/{playerId}")
-//	public @ResponseBody String getPlayerAvatar(@RequestHeader(required = true, value = "appId") String appId, @PathVariable String playerId, HttpServletResponse response) throws Exception {
-//		Player player = null;
-//
-//		try {
-//			String userId = getUserId();
-//			if (userId == null) {
-//				logger.error("User not found.");
-//				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//				return "";
-//			}
-//
-//			String gameId = appSetup.findAppById(appId).getGameId();
-//			if (gameId == null) {
-//				logger.error("GameId not found.");
-//				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//				return "";
-//			}
-//
-//			player = playerRepository.findByIdAndGameId(playerId, gameId);
-//
-//			if (player == null) {
-//				logger.error("Player not found.");
-//				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//				return "";
-//			}
-//
-//		} catch (Exception e) {
-//			logger.error("Error in get avatar: " + e.getMessage(), e);
-//			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//		}
-//
-//		return player.getAvatar();
-//	}
-
-	@GetMapping(value = "/gamificationweb/player/avatar/{playerId}") //, produces = org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public @ResponseBody void getPlayerAvatarData(@PathVariable String playerId, HttpServletResponse response) throws Exception {
-		Avatar avatar = avatarRepository.findOne(playerId);
-		
-		if (avatar != null) {
-			response.getOutputStream().write(avatar.getAvatarData().getData());
-			response.setContentLength(avatar.getAvatarData().getData().length);
-			response.setContentType(avatar.getContentType());
-//			response.setHeader("Content-Disposition", "inline; filename=\"" + avatar.getFileName() + "\"");
+	@GetMapping(value = "/gamificationweb/player/avatar/{appId}/{playerId}") //, produces = org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public @ResponseBody void getPlayerAvatarData(@PathVariable String appId, @PathVariable String playerId, HttpServletResponse response) throws Exception {
+		Avatar avatar = avatarRepository.findOne(playerId + "@" + appId);
+		if (avatar == null) {
+			avatar = avatarRepository.findOne(DEFAULT_USER);
 		}
+		
+		response.getOutputStream().write(avatar.getAvatarData().getData());
+		response.setContentLength(avatar.getAvatarData().getData().length);
+		response.setContentType(avatar.getContentType());
+
 	}	
 	
-	
-//	private void deleteFile(String dir, String name) {
-//		FileUtils.deleteQuietly(new File(dir, name));
-//	}
-//
-//	private String saveFile(String dir, String name, MultipartFile data) throws Exception {
-//		String fileName = name + "." + MediaType.parse(data.getContentType()).subtype();
-//
-//		File file = new File(dir, fileName);
-//		FileUtils.copyInputStreamToFile(data.getInputStream(), file);
-//
-//		return fileName;
-//	}
-
 	protected String getUserId() {
 		String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		return principal;
