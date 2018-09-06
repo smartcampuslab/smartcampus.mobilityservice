412b5e1a9719ec7528acbbf64824c12e353b1c04
gioadami <giordano.adami@gmail.com>
2018-09-06 11:03:30 +0200

    Removed 2.

--- src/main/java/eu/trentorise/smartcampus/mobility/service/SmartPlannerService.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/service/SmartPlannerService.java
@@ -111,8 +111,8 @@ public class SmartPlannerService implements SmartPlannerHelper {
 	
 	@PostConstruct
 	public void init() {
-		SMARTPLANNER = "/smart-planner2/" + smartplannerRouter + "/rest/";
-		OTP  = "/smart-planner2/" + smartplannerRouter + "/rest/";
+		SMARTPLANNER = "/smart-planner/" + smartplannerRouter + "/rest/";
+		OTP  = "/smart-planner/" + smartplannerRouter + "/rest/";
 	}
 	
 	@Override
