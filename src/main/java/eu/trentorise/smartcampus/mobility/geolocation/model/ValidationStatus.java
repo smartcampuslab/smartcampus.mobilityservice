cfcee8c20cb041b7443d911e5738c98228a21b3d
gioadami <giordano.adami@gmail.com>
2018-09-13 13:59:29 +0200

    Restored old setValidationOutcome (pending stay pending).

--- src/main/java/eu/trentorise/smartcampus/mobility/geolocation/model/ValidationStatus.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/geolocation/model/ValidationStatus.java
@@ -425,11 +425,12 @@ public class ValidationStatus {
 
 
 	public void setValidationOutcome(TravelValidity validationOutcome) {
-		if (TravelValidity.PENDING.equals(validationOutcome)) {
-			this.validationOutcome = TravelValidity.VALID;
-		} else {
-			this.validationOutcome = validationOutcome;
-		}
+//		if (TravelValidity.PENDING.equals(validationOutcome)) {
+//			this.validationOutcome = TravelValidity.VALID;
+//		} else {
+//			this.validationOutcome = validationOutcome;
+//		}
+		this.validationOutcome = validationOutcome;
 	}
 
 	public ERROR_TYPE getError() {
