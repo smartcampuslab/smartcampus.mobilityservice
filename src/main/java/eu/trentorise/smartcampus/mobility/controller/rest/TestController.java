ed1c6952cbcf79b32826d8df96cbfa2952ef15d2
gioadami <giordano.adami@gmail.com>
2018-10-26 16:39:54 +0200

    Disabled.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/TestController.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/TestController.java
@@ -1,20 +1,10 @@
 package eu.trentorise.smartcampus.mobility.controller.rest;
 
-import java.util.Map;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.common.collect.Maps;
-
-import eu.trentorise.smartcampus.communicator.model.Notification;
-import eu.trentorise.smartcampus.mobility.gamification.model.LevelGainedNotification;
 import eu.trentorise.smartcampus.mobility.gamificationweb.NotificationsManager;
 import eu.trentorise.smartcampus.mobility.service.NotificationHelper;
 import eu.trentorise.smartcampus.mobility.storage.PlayerRepositoryDao;
@@ -35,24 +25,24 @@ public class TestController {
 	
 	private static Log logger = LogFactory.getLog(TestController.class);
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/test/notification")
-	public @ResponseBody void notification(@RequestParam(required = true) String id, @RequestParam(required = false) String title, @RequestParam(required = false) String description, @RequestParam(required = false) String type, @RequestParam(required = true) String notificationAppId) throws Exception {
-		Notification notification = notificatioManager.buildSimpleNotification("it", LevelGainedNotification.class.getSimpleName());
-
-		if (title != null) {
-			notification.setTitle(title);
-		}
-		if (description != null) {
-			notification.setDescription(description);
-		}
-		if (type != null) {
-			Map<String, Object> content = Maps.newTreeMap();
-			content.put("type", type);
-			notification.setContent(content);
-		}
-		
-		notificatioHelper.notify(notification, id, notificationAppId);
-	}	
+//	@RequestMapping(method = RequestMethod.GET, value = "/test/notification")
+//	public @ResponseBody void notification(@RequestParam(required = true) String id, @RequestParam(required = false) String title, @RequestParam(required = false) String description, @RequestParam(required = false) String type, @RequestParam(required = true) String notificationAppId) throws Exception {
+//		Notification notification = notificatioManager.buildSimpleNotification("it", LevelGainedNotification.class.getSimpleName());
+//
+//		if (title != null) {
+//			notification.setTitle(title);
+//		}
+//		if (description != null) {
+//			notification.setDescription(description);
+//		}
+//		if (type != null) {
+//			Map<String, Object> content = Maps.newTreeMap();
+//			content.put("type", type);
+//			notification.setContent(content);
+//		}
+//		
+//		notificatioHelper.notify(notification, id, notificationAppId);
+//	}	
 	
 	
 //	@RequestMapping(method = RequestMethod.GET, value = "/test/broadcast")
