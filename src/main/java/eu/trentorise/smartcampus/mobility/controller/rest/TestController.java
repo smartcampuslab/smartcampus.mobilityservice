fe3ccbfb0aaa60ff820381bd73b01d03dda99e63
gioadami <giordano.adami@gmail.com>
2018-10-26 10:20:22 +0200

    Reenabled test controller for notifications.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/TestController.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/TestController.java
@@ -1,10 +1,20 @@
 package eu.trentorise.smartcampus.mobility.controller.rest;
 
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.common.collect.Maps;
+
+import eu.trentorise.smartcampus.communicator.model.Notification;
+import eu.trentorise.smartcampus.mobility.gamification.model.LevelGainedNotification;
 import eu.trentorise.smartcampus.mobility.gamificationweb.NotificationsManager;
 import eu.trentorise.smartcampus.mobility.service.NotificationHelper;
 import eu.trentorise.smartcampus.mobility.storage.PlayerRepositoryDao;
@@ -12,7 +22,7 @@ import eu.trentorise.smartcampus.mobility.storage.PlayerRepositoryDao;
 @Controller
 public class TestController {
 
-	private static final String NOTIFICATION_APP = "mobility.trentoplaygo.test";
+//	private static final String NOTIFICATION_APP = "mobility.trentoplaygo.test";
 	
 	@Autowired
 	private NotificationsManager notificatioManager;		
@@ -25,24 +35,24 @@ public class TestController {
 	
 	private static Log logger = LogFactory.getLog(TestController.class);
 	
-//	@RequestMapping(method = RequestMethod.GET, value = "/test/notification")
-//	public @ResponseBody void notification(@RequestParam(required = false) String id, @RequestParam(required = false) String title, @RequestParam(required = false) String description, @RequestParam(required = false) String type) throws Exception {
-//		Notification notification = notificatioManager.buildNotification("it", LevelGainedNotification.class.getSimpleName(), null);
-//
-//		if (title != null) {
-//			notification.setTitle(title);
-//		}
-//		if (description != null) {
-//			notification.setDescription(description);
-//		}
-//		if (type != null) {
-//			Map<String, Object> content = Maps.newTreeMap();
-//			content.put("type", type);
-//			notification.setContent(content);
-//		}
-//		
-//		notificatioHelper.notify(notification, (id == null ? "33" : id), NOTIFICATION_APP);
-//	}	
+	@RequestMapping(method = RequestMethod.GET, value = "/test/notification")
+	public @ResponseBody void notification(@RequestParam(required = true) String id, @RequestParam(required = false) String title, @RequestParam(required = false) String description, @RequestParam(required = false) String type, @RequestParam(required = true) String notificationAppId) throws Exception {
+		Notification notification = notificatioManager.buildSimpleNotification("it", LevelGainedNotification.class.getSimpleName());
+
+		if (title != null) {
+			notification.setTitle(title);
+		}
+		if (description != null) {
+			notification.setDescription(description);
+		}
+		if (type != null) {
+			Map<String, Object> content = Maps.newTreeMap();
+			content.put("type", type);
+			notification.setContent(content);
+		}
+		
+		notificatioHelper.notify(notification, id, notificationAppId);
+	}	
 	
 	
 //	@RequestMapping(method = RequestMethod.GET, value = "/test/broadcast")
