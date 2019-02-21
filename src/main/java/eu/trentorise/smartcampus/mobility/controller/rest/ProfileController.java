4e76a1e30e7ed69c841c6c03d8da10c911dda4d5
gioadami <giordano.adami@gmail.com>
2019-02-21 13:44:12 +0100

    Various changes.

--- src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/ProfileController.java
+++ src/main/java/eu/trentorise/smartcampus/mobility/controller/rest/ProfileController.java
@@ -2,7 +2,6 @@ package eu.trentorise.smartcampus.mobility.controller.rest;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
@@ -42,6 +41,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import eu.trentorise.smartcampus.mobility.gamification.model.TrackedInstance;
+import eu.trentorise.smartcampus.mobility.gamificationweb.EncryptDecrypt;
 import eu.trentorise.smartcampus.mobility.gamificationweb.model.Player;
 import eu.trentorise.smartcampus.mobility.gamificationweb.model.PlayerProfile;
 import eu.trentorise.smartcampus.mobility.gamificationweb.model.PlayerWaypoint;
@@ -54,10 +54,16 @@ import eu.trentorise.smartcampus.mobility.security.GameSetup;
 @RestController
 public class ProfileController {
 
+	@Value("${gamification.secretKey1}")
+	private String secretKey1;
+	@Autowired
+	@Value("${gamification.secretKey2}")
+	private String secretKey2;	
+	
 	private static final Logger logger = Logger.getLogger(ProfileController.class);
 
 	private static FastDateFormat shortSdfApi = FastDateFormat.getInstance("yyyy-MM-dd");
-//	private static FastDateFormat shortSdfDb = FastDateFormat.getInstance("yyyy/MM/dd");
+	private static FastDateFormat shortSdfDb = FastDateFormat.getInstance("yyyy/MM/dd");
 	private static FastDateFormat shortSdfGameInfo = FastDateFormat.getInstance("dd/MM/yyyy");
 	private static FastDateFormat extendedSdf = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss,Z");
 	private static FastDateFormat monthSdf = FastDateFormat.getInstance("yyyy-MM");
@@ -76,6 +82,8 @@ public class ProfileController {
 
 	@Autowired
 	private GameSetup gameSetup;
+	
+	private EncryptDecrypt cryptUtils;
 
 	@GetMapping("/profile/{campaignId}")
 	public @ResponseBody List<PlayerProfile> profile(@PathVariable String campaignId, @RequestParam(required = false) String date_from, @RequestParam(required = false) String date_to)
@@ -112,29 +120,57 @@ public class ProfileController {
 		return profiles;
 	}
 
-	@Scheduled(cron="0 30 17 * * *")
+	@Scheduled(cron="0 0 14 * * *")
+//	@PostConstruct
 	public void generateWaypoints() throws Exception {
-		List<String> campaignIds = appSetup.getApps().stream().map(x -> x.getAppId()).collect(Collectors.toList());
-		for (String campaignId: campaignIds) {
-			generateWaypoints(campaignId);
-		}
+		cryptUtils = new EncryptDecrypt(secretKey1, secretKey2);
+		
+		Runnable r= new Runnable() {
+			
+			@Override
+			public void run() {
+				logger.info("Starting waypoints generation");
+				List<String> campaignIds = appSetup.getApps().stream().map(x -> x.getAppId()).collect(Collectors.toList());
+				for (String campaignId: campaignIds) {
+					try {
+						generateWaypoints(campaignId);
+					} catch (Exception e) {
+						logger.info("Error generating waypoints");
+					}
+				}
+				logger.info("Ended waypoints generation");
+			}
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+		
 	}
 	
 	
 	public void generateWaypoints(String campaignId) throws Exception {
-		SortedSet<String> months = findMonths(campaignId);
-		String currentMonth = monthSdf.format(new Date());
-		
 		AppInfo app = appSetup.findAppById(campaignId);
 		String gameId = app.getGameId();
 		if (gameId == null) {
 			return;
 		}
+		
+		Stopwatch sw = Stopwatch.createStarted();
+		int created = 0;
+		
 		logger.info("Generating waypoints for " + campaignId);
+
+		Date startGame = shortSdfGameInfo.parse(gameSetup.findGameById(gameId).getStart());		
+		
+		String currentMonth = monthSdf.format(new Date());
+		String gameStart = shortSdfDb.format(startGame);		
+		
+		SortedSet<String> months = findMonths(campaignId, gameStart);		
 		
 		Criteria criteria0 = new Criteria("gameId").is(gameId);
 		Query query0 = new Query(criteria0);
 		List<Player> players = template.find(query0, Player.class);
+//		List<Player> players = template.findAll(Player.class);
 		Collections.sort(players, new Comparator<Player>() {
 
 			@Override
@@ -143,15 +179,22 @@ public class ProfileController {
 			}
 		});
 		
-		
 		for (String month: months) {
-			createMissingMonth(month, currentMonth, campaignId, players);
+			created += createMissingMonth(month, currentMonth, campaignId, null);
 		}
+		
+		sw.stop();
+		logger.info("Total Waypoints generated: " + created + ", time elapsed: " + sw.elapsed(TimeUnit.SECONDS));
+		
 	}
 
 	private PlayerProfile convertPlayer(Player p, int year) {
 		PlayerProfile pp = new PlayerProfile();
-		pp.setUser_id(p.getPlayerId());
+		try {
+			pp.setUser_id(cryptUtils.encrypt(p.getPlayerId()));
+		} catch (Exception e) {
+			logger.error("Error encrypting player id", e);
+		}
 		Long rd = (Long) p.getPersonalData().getOrDefault("timestamp", 0);
 		pp.setDate_of_registration(shortSdfApi.format(rd));
 
@@ -161,13 +204,12 @@ public class ProfileController {
 	}
 
 	private void completeWithSurvey(PlayerProfile pp, Player p, int year) {
-		;
 		if (!p.getSurveys().containsKey("start")) {
 			return;
 		}
 		Map<String, Object> survey = p.getSurveys().get("start");
 		String gender;
-		switch ((String) survey.get("gender")) {
+		switch ((String) survey.getOrDefault("gender", "")) {
 		case "maschio":
 			gender = "male";
 			break;
@@ -179,45 +221,51 @@ public class ProfileController {
 		}
 		pp.setGender(gender);
 
-		String age = (String) survey.get("age");
-		if (age.contains("+")) {
-			pp.setYear_of_birth_max(findYear(age.replace("+", ""), year));
-		} else {
-			String years[] = age.split("-");
-			pp.setYear_of_birth_min(findYear(years[1], year));
-			pp.setYear_of_birth_max(findYear(years[0], year));
+		if (survey.containsKey("age")) {
+			String age = (String) survey.get("age");
+			if (age.contains("+")) {
+				pp.setYear_of_birth_max(findYear(age.replace("+", ""), year));
+			} else {
+				String years[] = age.split("-");
+				pp.setYear_of_birth_min(findYear(years[1], year));
+				pp.setYear_of_birth_max(findYear(years[0], year));
+			}
 		}
 
-		String mean = null;
-		switch ((String) survey.get("mean")) {
-		case "auto":
-			mean = "private car";
-			break;
-		case "public":
-			mean = "public transport";
-			break;
-		case "bike":
-			mean = "bike";
-			break;
-		case "walk":
-			mean = "walking";
-			break;
-		case "bikesharing":
-			mean = "bike sharing";
-			break;
-		case "carsharing":
-			mean = "car sharing/pooling";
-			break;
+		if (survey.containsKey("mean")) {
+			String mean = null;
+			switch ((String) survey.get("mean")) {
+			case "auto":
+				mean = "private car";
+				break;
+			case "public":
+				mean = "public transport";
+				break;
+			case "bike":
+				mean = "bike";
+				break;
+			case "walk":
+				mean = "walking";
+				break;
+			case "bikesharing":
+				mean = "bike sharing";
+				break;
+			case "carsharing":
+				mean = "car sharing/pooling";
+				break;
+			}
+			pp.setMain_mode_pre_campaign(mean);
 		}
-		pp.setMain_mode_pre_campaign(mean);
 
-		String kms = (String) survey.get("kms");
-		if (kms.contains("+")) {
-			pp.setDaily_travelled_distance_min(kms.replace("+", ""));
-		} else {
-			String km[] = kms.split("-");
-			pp.setDaily_travelled_distance_min(km[0]);
-			pp.setDaily_travelled_distance_max(km[1]);
+		if (survey.containsKey("kms")) {
+			String kms = (String) survey.get("kms");
+			if (kms.contains("+")) {
+				pp.setDaily_travelled_distance_min(kms.replace("+", ""));
+			} else {
+				String km[] = kms.split("-");
+				pp.setDaily_travelled_distance_min(km[0]);
+				pp.setDaily_travelled_distance_max(km[1]);
+			}
 		}
 	}
 
@@ -225,20 +273,20 @@ public class ProfileController {
 		return "" + (year - Integer.parseInt(age));
 	}
 
-	private void createMissingMonth(String date, String currentDate, String campaignId, List<Player> players) throws Exception {
+	private int createMissingMonth(String date, String currentDate, String campaignId, List<Player> players) throws Exception {
 		List<PlayerWaypoints> result = Lists.newArrayList();
 
 		String from = date + "/01";
 		String to = date + "/31";
 		
 		String suffix = date.replace("/", "-");
-		File f = new File(waypointsDir + "/waypoints_" + campaignId + "_" + suffix + ".zip");
+		File f = new File(waypointsDir + "/" + campaignId + "_" + suffix + ".zip");
 		if (f.exists()) {
 			if (suffix.equals(currentDate)) {
 				logger.info("Overwriting current waypoints " + suffix);
 			} else {
 				logger.info("Skipping existing waypoints " + suffix);
-				return;
+				return 0;
 			}
 		} else {
 			logger.info("Writing missing waypoints " + suffix);
@@ -248,15 +296,32 @@ public class ProfileController {
 		
 		FileOutputStream fos0 = new FileOutputStream(f);
 		ZipOutputStream fos = new ZipOutputStream(fos0);
-		ZipEntry ze = new ZipEntry("waypoints_" + campaignId + "_" + suffix + ".json");
+		ZipEntry ze = new ZipEntry(campaignId + "_" + suffix + ".json");
 		fos.putNextEntry(ze);
 		
 		ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
 		try (SequenceWriter sequenceWriter = writer.writeValues(fos)) {
 			sequenceWriter.init(true);
 			
+			if (players != null) {
 			for (Player p : players) {
 				Criteria criteria = new Criteria("appId").is(campaignId).and("userId").is(p.getPlayerId()).and("freeTrackingTransport").ne(null);
+				Criteria criteriaFrom = new Criteria("day").gte(from);
+				Criteria criteriaTo = new Criteria("day").lte(to);
+				
+				criteria = criteria.andOperator(criteriaFrom, criteriaTo);
+
+				Query query = new Query(criteria);
+
+				CloseableIterator<TrackedInstance> it = template.stream(query, TrackedInstance.class);
+				while (it.hasNext()) {
+					TrackedInstance ti = it.next();
+					PlayerWaypoints pws = convertTrackedInstance(ti, sequenceWriter);
+					result.add(pws);
+				}
+			}
+			} else {
+				Criteria criteria = new Criteria("appId").is(campaignId).and("freeTrackingTransport").ne(null);
 				Criteria criteriaFrom = new Criteria("day").gte(from);
 				Criteria criteriaTo = new Criteria("day").lte(to);
 				
@@ -275,10 +340,11 @@ public class ProfileController {
 
 		sw.stop();
 		logger.info("Waypoints generated: " + result.size() + ", time elapsed: " + sw.elapsed(TimeUnit.SECONDS));
+		return result.size();
 	}
 	
-	private SortedSet<String> findMonths(String appId) {
-		Criteria criteria = new Criteria("appId").is(appId).and("freeTrackingTransport").ne(null);
+	private SortedSet<String> findMonths(String appId, String gameStart) {
+		Criteria criteria = new Criteria("appId").is(appId).and("freeTrackingTransport").ne(null).and("day").gte(gameStart);
 		Query query = new Query(criteria);
 		query.fields().include("day");
 		
@@ -287,10 +353,14 @@ public class ProfileController {
 		return sortedDates;
 	}	
 
-	private PlayerWaypoints convertTrackedInstance(TrackedInstance ti, SequenceWriter writer) throws IOException {
+	private PlayerWaypoints convertTrackedInstance(TrackedInstance ti, SequenceWriter writer) throws Exception {
 		PlayerWaypoints pws = new PlayerWaypoints();
 
-		pws.setUser_id(ti.getUserId());
+		try {
+			pws.setUser_id(cryptUtils.encrypt(ti.getUserId()));
+		} catch (Exception e) {
+			logger.error("Error encrypting player id", e);
+		}
 		pws.setActivity_id(ti.getId());
 		pws.setActivity_type(ti.getFreeTrackingTransport());		
 		for (Geolocation loc : ti.getGeolocationEvents()) {
