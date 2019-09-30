d09510fb7f0fe77c999c2b5648dfa1090825d1a4
kazhamiakin <kazhamiakin@gmail.com>
2019-09-30 13:11:49 +0200

    minor fixes

--- src/main/webapp/js/props.js
+++ src/main/webapp/js/props.js
@@ -1,10 +1,10 @@
 MAIL='webplanner@smartcommunitylab.it';
 
 GEOCODER = 'https://os.smartcommunitylab.it/core.geocoder/';
-PARKING = 'https://dev.smartcommunitylab.it/core.mobility/';
-TAXI = 'https://dev.smartcommunitylab.it/core.mobility/';
+PARKING = 'https://tn.smartcommunitylab.it/core.mobility/';
+TAXI = 'https://tn.smartcommunitylab.it/core.mobility/';
 BIKESHARING = 'https://tn.smartcommunitylab.it/core.mobility/';
-PLANNER = 'http://localhost:1010/core.mobility';
+PLANNER = 'https://tn.smartcommunitylab.it/core.mobility';
 PARKING_AGENCIES = ['COMUNE_DI_TRENTO','COMUNE_DI_ROVERETO'];
 EXTRAURBAN_AGENCIES=['17'];
 BIKE_AGENCIES = ['trento','rovereto', 'pergine_valsugana', 'lavis', 'mezzocorona', 'mezzolombardo', 'sanmichelealladige'];
